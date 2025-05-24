f80819a41dbde12506e3f21617dac2fab4eaf559
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-05-24 12:29:37 +0100

    QUestion 1084 and answer to the other questions updated

--- 24May2025LeetCode.sql
+++ 24May2025LeetCode.sql
@@ -32,6 +32,15 @@ From 14ProductSalesAnalysis1_1068 s
 Left Join 14_1Product_1068 p
 on s.product_id = p.product_id;
 
++--------------+------+-------+
+| product_name | year | price |
++--------------+------+-------+
+| Nokia        | 2008 |  5000 |
+| Nokia        | 2009 |  5000 |
+| Apple        | 2011 |  9000 |
++--------------+------+-------+
+
+
 Q.1075 Project Employees I
 
 Create table if not exists 15_1Project_1075 (
@@ -68,3 +77,57 @@ From 15_1Project_1075 p1
 Left Join 15ProjectEmployeesI_1075 p2
 on p1.employee_id = p2.employee_id
 group by p1.project_id;
+
++------------+----------------+
+| project_id | avgerage_years |
++------------+----------------+
+|          1 |           2.00 |
+|          2 |           2.50 |
++------------+----------------+
+
+Q.1084 Sales Analysis I
+
+CREATE TABLE 16Product1084 (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(255),
+    unit_price INT
+);
+
+CREATE TABLE 16_1Sales1084 (
+    seller_id INT,
+    product_id INT,
+    buyer_id INT,
+    sale_date DATE,
+    quantity INT,
+    price INT,
+    FOREIGN KEY (product_id) REFERENCES 16Product1084 (product_id)
+);
+
+INSERT INTO 16Product1084 (product_id, product_name, unit_price) VALUES
+(1, 'iPhone', 999),
+(2, 'Galaxy', 799),
+(3, 'Pixel', 699);
+
+INSERT INTO 16_1Sales1084 (seller_id, product_id, buyer_id, sale_date, quantity, price) VALUES
+(101, 1, 201, '2019-01-21', 1, 999),
+(102, 2, 202, '2019-02-17', 2, 1598),
+(103, 3, 203, '2019-06-02', 1, 699),
+(101, 1, 204, '2019-05-13', 1, 999);
+
+
+Select * from 16_1Sales1084;
+
+Select * from 16Product1084;
+
+Select p.product_id, p.product_name
+From 16Product1084 P
+left Join 16_1Sales1084 s
+on p.product_id = s.product_id 
+group by p.product_id
+Having Min(s.sale_date) >= '2019-01-01' AND Max(s.sale_date) <= '2019-03-31';
+
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+|          2 | Galaxy       |
++------------+--------------+
