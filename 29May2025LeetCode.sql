5f4010670242483a5f90d7c744d10237822ee0cd
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-05-29 17:34:31 +0100

    Solved question 1517 on leetcode

--- 29May2025LeetCode.sql
+++ 29May2025LeetCode.sql
@@ -284,6 +284,52 @@ Output
 
 --------------------------------------------------------------------------
 
+Q.1517 Find users with valid e-email
+
+Create table if not exists 27FindUsersWithVlaidEMail1517
+(
+    user_id int primary key,
+    name varchar(100),
+    email varchar(100)
+);
+
+insert into 27FindUsersWithVlaidEMail1517 values
+(1, 'Winston', 'winston@leetcode.com'),
+(2, 'Jonathan', 'jonathanisgreat'),
+(3, 'Annabelle', 'bella-@leetcode.com'),
+(4, 'Sally', 'sally.come@leetcode.com'),
+(5, 'Marwan', 'quarz#2020@leetcode.com'),
+(6, 'David', 'david69@gmail.com'),
+(7, 'Shapiro', '.shapo@leetcode.com');
+
+Select * from 27FindUsersWithVlaidEMail1517;
+
+--Write a solution to find the users who have valid emails.
+
+-- A valid e-mail has a prefix name and a domain where:
+
+-- The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+--The domain is '@leetcode.com'.
+-- Return the result table in any order.
+
+
+Select * 
+from 27FindUsersWithVlaidEMail1517
+where email REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com';
+
+
++---------+-----------+-------------------------+
+| user_id | name      | email                   |
++---------+-----------+-------------------------+
+|       1 | Winston   | winston@leetcode.com    |
+|       3 | Annabelle | bella-@leetcode.com     |
+|       4 | Sally     | sally.come@leetcode.com |
++---------+-----------+-------------------------+
+
+
+
+
+
 
 
 
