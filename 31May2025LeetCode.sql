36f2b0fe32cb33322091e5bdb53f7f30701286c1
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-05-31 12:11:10 +0100

    Solved 1633 from leetcode

--- 31May2025LeetCode.sql
+++ 31May2025LeetCode.sql
@@ -138,3 +138,89 @@ Output
 --------------------------------------------------------------------------
 
 
+
+Q.1633 Percentage of Users Attended a Contest
+
+Create table if not exists 31PercentOfUsersAttendContest1633(
+    user_id int Primary Key,
+    user_name varchar(50)
+);
+
+insert into 31PercentOfUsersAttendContest1633 values
+(6, 'Alice'),
+(2, 'Bob'),
+(7, 'Alex');
+
+create table if not exists 31_1Register1633(
+    contest_id int,
+    user_id int,
+    Primary key (contest_id, user_id)
+);
+
+insert into 31_1Register1633 values
+(215, 6),
+(209, 2),
+(208, 2),
+(210, 6),
+(208, 6),
+(209, 7),
+(209, 6),
+(215, 7),
+(208, 7),
+(210, 2),
+(207, 2),
+(210, 7);
+
+
+mysql> select * from 31PercentOfUsersAttendContest1633;
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+|       2 | Bob       |
+|       6 | Alice     |
+|       7 | Alex      |
++---------+-----------+
+3 rows in set (0.00 sec)
+
+mysql> Select * from 31_1Register1633;
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+|        207 |       2 |
+|        208 |       2 |
+|        208 |       6 |
+|        208 |       7 |
+|        209 |       2 |
+|        209 |       6 |
+|        209 |       7 |
+|        210 |       2 |
+|        210 |       6 |
+|        210 |       7 |
+|        215 |       6 |
+|        215 |       7 |
++------------+---------+
+
+
+--Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+--Return the result table ordered by percentage in descending order. 
+--In case of a tie, order it by contest_id in ascending order.
+
+select 
+contest_id, 
+round(count(distinct user_id) * 100 /(select count(user_id) from 31PercentOfUsersAttendContest1633) ,2) as percentage
+from  31_1Register1633
+group by contest_id
+order by percentage desc,contest_id;
+
+
+Output
+
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+|        208 |  100.00000 |
+|        209 |  100.00000 |
+|        210 |  100.00000 |
+|        215 |   66.66667 |
+|        207 |   33.33333 |
++------------+------------+
