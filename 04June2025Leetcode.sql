8a2e4813eeda98642428f5250f1f5a8acca8a0ed
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-04 11:13:30 +0100

    Solved question 1757 on leetcode

--- 04June2025Leetcode.sql
+++ 04June2025Leetcode.sql
@@ -122,3 +122,54 @@ Output
 
 --------------------------------------------------------------------------
 
+
+Q. 1757 Recyclable and low fat products
+
+Create table if not exists 37RecyclableLowFatProduct1757(
+    product_id int primary key,
+    low_fats enum('Y', 'N'),
+    recyclable enum('Y', 'N')
+);
+
+insert into 37RecyclableLowFatProduct1757 values 
+(0, 'Y', 'N'),
+(1, 'Y', 'Y'),
+(2, 'N', 'Y'),
+(3, 'Y', 'Y'),
+(4, 'N', 'N');
+
+
+mysql> select * from 37RecyclableLowFatProduct1757;
++------------+----------+------------+
+| product_id | low_fats | recyclable |
++------------+----------+------------+
+|          0 | Y        | N          |
+|          1 | Y        | Y          |
+|          2 | N        | Y          |
+|          3 | Y        | Y          |
+|          4 | N        | N          |
++------------+----------+------------+
+
+
+--Write a solution to find the ids of products that are both low fat and recyclable.
+--Return the result table in any order.
+
+
+select product_id
+from 37RecyclableLowFatProduct1757
+where low_fats = 'Y' AND 
+recyclable = 'Y'
+
+
++------------+
+| product_id |
++------------+
+|          1 |
+|          3 |
++------------+
+
+
+--------------------------------------------------------------------------
+
+
+
