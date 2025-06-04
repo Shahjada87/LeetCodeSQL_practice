b903688ea5525ebe37f002a38a500cea393e7c82
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-04 11:05:54 +0100

    solved question 1741 on leetcode

--- 04June2025Leetcode.sql
+++ 04June2025Leetcode.sql
@@ -52,3 +52,73 @@ Select activity_date, count(DISTINCT user_id)  as active_users
 from 7UserActivityPast30DaysI1141
 where activity_date BETWEEN DATE_ADD('2019-07-27', interval -29 day) AND '2019-07-27'
 group by activity_date;
+
+Output
+
++---------------+--------------+
+| activity_date | active_users |
++---------------+--------------+
+| 2019-07-20    |            2 |
+| 2019-07-21    |            2 |
++---------------+--------------+
+2 rows in set (0.10 sec)
+
+
+--------------------------------------------------------------------------
+
+show tables;
+
+Q. 1741 Find total time spent by each employee
+
+
+create table if not exists 36TimeSpentByEmployee1741(
+    emp_id int,
+    event_day date,
+    in_time int,
+    out_time int,
+    Primary key (emp_id, event_day, in_time)
+);
+
+insert into 36TimeSpentByEmployee1741 values
+(1, '2020-11-28', 4, 32),
+(1, '2020-11-28', 55, 200),
+(1, '2020-12-03', 1, 42),
+(2, '2020-11-28', 3, 33),
+(2, '2020-12-09', 47, 74);
+
+
+mysql> select * from 36TimeSpentByEmployee1741;
++--------+------------+---------+----------+
+| emp_id | event_day  | in_time | out_time |
++--------+------------+---------+----------+
+|      1 | 2020-11-28 |       4 |       32 |
+|      1 | 2020-11-28 |      55 |      200 |
+|      1 | 2020-12-03 |       1 |       42 |
+|      2 | 2020-11-28 |       3 |       33 |
+|      2 | 2020-12-09 |      47 |       74 |
++--------+------------+---------+----------+
+
+
+--Write a solution to calculate the total time in minutes spent by each employee on each day at the office. 
+--Note that within one day, an employee can enter and leave more than once.
+--The time spent in the office for a single entry is out_time - in_time.
+--Return the result table in any order.
+
+
+select event_day as day, emp_id, sum(out_time - in_time) as total_time
+From 36TimeSpentByEmployee1741
+group by event_day, emp_id;
+
+Output
+
++------------+--------+------------+
+| day        | emp_id | total_time |
++------------+--------+------------+
+| 2020-11-28 |      1 |        173 |
+| 2020-12-03 |      1 |         41 |
+| 2020-11-28 |      2 |         30 |
+| 2020-12-09 |      2 |         27 |
++------------+--------+------------+
+
+--------------------------------------------------------------------------
+
