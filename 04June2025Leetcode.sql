23f9873cbd3853d0049f707f19a91466e4ce6d45
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-04 13:40:26 +0100

    solved 1789 on leetcode

--- 04June2025Leetcode.sql
+++ 04June2025Leetcode.sql
@@ -172,4 +172,63 @@ recyclable = 'Y'
 --------------------------------------------------------------------------
 
 
+Q.1789 Primary department for each employee
+
+create table if not exists 38PrimaryDepartmentOfEachEmp1789(
+    employee_id int,
+    department_id int,
+    primary_flag varchar(50),
+    Primary key (employee_id, department_id)
+);
+
+insert into 38PrimaryDepartmentOfEachEmp1789 values
+(1, 1, 'N'),
+(2, 1, 'Y'),
+(2, 2, 'N'),
+(3, 3, 'N'),
+(4, 2, 'N'),
+(4, 3, 'Y'),
+(4, 4, 'N');
+
+mysql> select * from 38PrimaryDepartmentOfEachEmp1789;
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+|           1 |             1 | N            |
+|           2 |             1 | Y            |
+|           2 |             2 | N            |
+|           3 |             3 | N            |
+|           4 |             2 | N            |
+|           4 |             3 | Y            |
+|           4 |             4 | N            |
++-------------+---------------+--------------+
+
+
+--Employees can belong to multiple departments. When the employee joins other departments, 
+--they need to decide which department is their primary department. 
+--Note that when an employee belongs to only one department, their primary column is 'N'.
+--Write a solution to report all the employees with their primary department. 
+--For employees who belong to one department, report their only department.
+--Return the result table in any order.
+
+
+SELECT employee_id, 
+CASE WHEN COUNT(department_id) = 1 THEN MAX(department_id)
+ELSE MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+END AS department_id
+FROM 38PrimaryDepartmentOfEachEmp1789
+GROUP BY employee_id;
+
+Output
+
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+|           1 |             1 |
+|           2 |             1 |
+|           3 |             3 |
+|           4 |             3 |
++-------------+---------------+
+4 rows in set (0.06 sec)
+
 
