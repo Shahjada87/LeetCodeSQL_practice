c30b120d7af61e69c4904fc7701409858e8b3596
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-05 07:47:13 +0100

    solved question no. 1890

--- 14.05June2025LeetCode.sql
+++ 14.05June2025LeetCode.sql
@@ -202,3 +202,80 @@ else 0 end as bonus
 from 40CalculateSpecialBonus1873
 order by employee_id;
 
+Output 
+
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+|           2 |     0 |
+|           3 |     0 |
+|           7 |  7400 |
+|           8 |     0 |
+|           9 |  7700 |
++-------------+-------+
+
+
+--------------------------------------------------------------------------
+
+Q. 1890 The latest login in 2020
+
+create table if not exists 41LatestLoginIn2020year1890(
+    user_id int,
+    time_stamp date,
+    Primary Key (user_id,time_stamp)
+)
+
+Alter table 41LatestLoginIn2020year1890 
+modify column time_stamp datetime;
+
+Delete from 41LatestLoginIn2020year1890;
+
+
+Insert into 41LatestLoginIn2020year1890 values 
+(6, '2020-06-30 15:06:07'),
+(6, '2021-04-21 14:06:06'),
+(6, '2019-03-07 00:18:15'),
+(8, '2020-02-01 05:10:53'),
+(8, '2020-12-30 00:46:50'),
+(2, '2020-01-16 02:49:50'),
+(2, '2019-08-25 07:59:08'),
+(14, '2019-07-14 09:00:00'),
+(14, '2021-01-06 11:59:59');
+
+
+mysql> select * from 41LatestLoginIn2020year1890;
++---------+---------------------+
+| user_id | time_stamp          |
++---------+---------------------+
+|       2 | 2019-08-25 07:59:08 |
+|       2 | 2020-01-16 02:49:50 |
+|       6 | 2019-03-07 00:18:15 |
+|       6 | 2020-06-30 15:06:07 |
+|       6 | 2021-04-21 14:06:06 |
+|       8 | 2020-02-01 05:10:53 |
+|       8 | 2020-12-30 00:46:50 |
+|      14 | 2019-07-14 09:00:00 |
+|      14 | 2021-01-06 11:59:59 |
++---------+---------------------+
+
+
+--Write a solution to report the latest login for all users in the year 2020. 
+--Do not include the users who did not login in 2020.
+--Return the result table in any order.
+
+select user_id, max(time_stamp) as last_stamp
+from 41LatestLoginIn2020year1890 
+where year(time_stamp) = 2020
+GROUP BY user_id;
+
+
+Output
+
++---------+---------------------+
+| user_id | last_stamp          |
++---------+---------------------+
+|       2 | 2020-01-16 02:49:50 |
+|       6 | 2020-06-30 15:06:07 |
+|       8 | 2020-12-30 00:46:50 |
++---------+---------------------+
+
