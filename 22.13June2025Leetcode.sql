b3192a24d47616008a4ed06e39635783e164510d
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-13 16:46:21 +0100

    Solved 1581 again from leetcode

--- 22.13June2025Leetcode.sql
+++ 22.13June2025Leetcode.sql
@@ -69,3 +69,84 @@ Output
 --------------------------------------------------------------------------
 
 
+
+Q. 1581 Customer Who Visited but Did Not Make Any Transactions
+
+create table if not exists 15CustomerVsistedButDidNotMakeTransactions1581(
+    visitor_id int primary key,
+    customer_id int
+);
+
+
+insert into 15CustomerVsistedButDidNotMakeTransactions1581 values 
+(1, 23),
+(2, 9),
+(4, 30),
+(5, 54),
+(6, 96),
+(7, 54),
+(8, 54);
+
+mysql> select * from 15CustomerVsistedButDidNotMakeTransactions1581;
++------------+-------------+
+| visitor_id | customer_id |
++------------+-------------+
+|          1 |          23 |
+|          2 |           9 |
+|          4 |          30 |
+|          5 |          54 |
+|          6 |          96 |
+|          7 |          54 |
+|          8 |          54 |
++------------+-------------+
+
+
+
+create table if not exists 15_1Transactions1581(
+    transaction_id int primary key,
+    visit_id int,
+    amount int
+)
+
+insert into 15_1Transactions1581 values 
+(2, 5, 310),
+(3, 5, 300),
+(9, 5, 200),
+(12, 1, 910),
+(13, 2, 970);
+
+mysql> select * from 15_1Transactions1581;
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+|              2 |        5 |    310 |
+|              3 |        5 |    300 |
+|              9 |        5 |    200 |
+|             12 |        1 |    910 |
+|             13 |        2 |    970 |
++----------------+----------+--------+
+
+
+--Write a solution to find the IDs of the users who visited without making any transactions and 
+--the number of times they made these types of visits.
+--Return the result table sorted in any order.
+
+Select a.customer_id, count(a.visitor_id) as count_no_trans
+from 15CustomerVsistedButDidNotMakeTransactions1581 a
+left join 15_1Transactions1581 b
+on a.visitor_id = b.visit_id
+where b.transaction_id IS NULL
+group by a.customer_id;
+
+
+Output
+
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+|          30 |              1 |
+|          96 |              1 |
+|          54 |              2 |
++-------------+----------------+
+
+
