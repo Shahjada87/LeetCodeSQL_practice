80857f78917a8580ebf733f806690aeb1bb7cdb7
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-06-18 21:06:20 +0100

    solved a medium level question on leetcode question 176

---
+++ 25.18June2025Leetcode.sql
@@ -0,0 +1,51 @@
+
+show tables;
+
+Q. 176 Second highest salary
+
+create table if not exists 47SecondHighestSalary176(
+    id int primary key,
+    salary int
+);
+
+insert into 47SecondHighestSalary176 VALUES
+(1,100),
+(2,200),
+(3,300);
+
+
+mysql> select * from 47SecondHighestSalary176;
++----+--------+
+| id | salary |
++----+--------+
+|  1 |    100 |
+|  2 |    200 |
+|  3 |    300 |
++----+--------+
+
+
+--Write a solution to find the second highest distinct salary from the Employee table. 
+--If there is no second highest salary, return null (return None in Pandas).
+
+select
+(select distinct Salary 
+from 47SecondHighestSalary176 order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary
+
+
+Output
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+|                 200 |
++---------------------+
+
+
+--------------------------------------------------------------------------
+
+
+
+
+
