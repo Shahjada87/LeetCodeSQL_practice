4bea9a80aae107a935bb3399029f4b07041221d8
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-07-10 16:20:50 +0100

    solved 1327 List the Products Ordered in a Period..

--- 42.10July2025Leetcode.sql
+++ 42.10July2025Leetcode.sql
@@ -82,3 +82,110 @@ Output
 |          2 |         16.96 |
 +------------+---------------+
 2 rows in set (0.02 sec)
+
+
+-----------------------------------------------------------------------------------------------
+
+
+Q.1327 List the Products Ordered in a Period
+
+create table if not exists 34ProductsOrderedInAPeriod1327(
+    product_id int primary key,
+    product_name varchar(50),
+    product_category varchar(200)
+);
+
+
+insert into 34ProductsOrderedInAPeriod1327 values
+(1, 'Leetcode Solutions', 'Book'),
+(2, 'Jewels of Stringology', 'Book'),
+(3, 'HP', 'Laptop'),
+(4, 'Lenovo', 'Laptop'),
+(5, 'Leetcode Kit', 'T-shirt');
+
+
+mysql> select * from 34ProductsOrderedInAPeriod1327;
++------------+-----------------------+------------------+
+| product_id | product_name          | product_category |
++------------+-----------------------+------------------+
+|          1 | Leetcode Solutions    | Book             |
+|          2 | Jewels of Stringology | Book             |
+|          3 | HP                    | Laptop           |
+|          4 | Lenovo                | Laptop           |
+|          5 | Leetcode Kit          | T-shirt          |
++------------+-----------------------+------------------+
+5 rows in set (0.00 sec)
+
+
+create table if not exists 34_1Orders1327(
+    product_id int,
+    order_date date,
+    unit int,
+    Foreign key (product_id) references 34ProductsOrderedInAPeriod1327(product_id)
+);
+
+insert into 34_1Orders1327 values
+(1, '2020-02-05', 60),
+(1, '2020-02-10', 70),
+(2, '2020-01-18', 30),
+(2, '2020-02-11', 80),
+(3, '2020-02-17', 2),
+(3, '2020-02-24', 3),
+(4, '2020-03-01', 20),
+(4, '2020-03-04', 30),
+(4, '2020-03-04', 60),
+(5, '2020-02-25', 50),
+(5, '2020-02-27', 50),
+(5, '2020-03-01', 50);
+
+
+
+mysql> select * from 34_1Orders1327;
++------------+------------+------+
+| product_id | order_date | unit |
++------------+------------+------+
+|          1 | 2020-02-05 |   60 |
+|          1 | 2020-02-10 |   70 |
+|          2 | 2020-01-18 |   30 |
+|          2 | 2020-02-11 |   80 |
+|          3 | 2020-02-17 |    2 |
+|          3 | 2020-02-24 |    3 |
+|          4 | 2020-03-01 |   20 |
+|          4 | 2020-03-04 |   30 |
+|          4 | 2020-03-04 |   60 |
+|          5 | 2020-02-25 |   50 |
+|          5 | 2020-02-27 |   50 |
+|          5 | 2020-03-01 |   50 |
++------------+------------+------+
+12 rows in set (0.01 sec)
+
+
+
+--Write a solution to get the names of products that have at least 100 units 
+--ordered in February 2020 and their amount.
+--Return the result table in any order.
+
+
+with cte as 
+(select product_id, sum(unit) as unit
+from 34_1Orders1327
+where Year(order_date) = 2020
+AND MONTH(order_date) = 02
+group by product_id)
+select a.product_name, b.unit
+from 34ProductsOrderedInAPeriod1327 a
+left join cte b
+on a.product_id = b.product_id
+where b.unit >= 100
+
+
+Output 
+
+
++--------------------+------+
+| product_name       | unit |
++--------------------+------+
+| Leetcode Solutions |  130 |
+| Leetcode Kit       |  100 |
++--------------------+------+
+2 rows in set (0.07 sec)
