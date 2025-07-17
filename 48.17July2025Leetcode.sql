a896b009495011b7ccd1d889664f1811a90d5380
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-07-17 12:11:51 +0100

    solved Q597 Friend Request acceptance rate on leetcode

---
+++ 48.17July2025Leetcode.sql
@@ -0,0 +1,105 @@
+
+use leetcode;
+
+show tables;
+
+
+Q. 597 Friend Requests I: Overall acceptance rate
+
+create table 50FrndRqstOverallAcceptanceRate597(
+    sender_id int,
+    send_to_id int,
+    request_date date
+);
+
+insert into 50FrndRqstOverallAcceptanceRate597 values
+(1, 2, '2016-06-01'),
+(1, 3, '2016-06-01'),
+(1, 4, '2016-06-01'),
+(2, 3, '2016-06-02'),
+(3, 4, '2016-06-09');
+
+
+mysql> select * from 50FrndRqstOverallAcceptanceRate597;
++-----------+------------+--------------+
+| sender_id | send_to_id | request_date |
++-----------+------------+--------------+
+|         1 |          2 | 2016-06-01   |
+|         1 |          3 | 2016-06-01   |
+|         1 |          4 | 2016-06-01   |
+|         2 |          3 | 2016-06-02   |
+|         3 |          4 | 2016-06-09   |
++-----------+------------+--------------+
+5 rows in set (0.01 sec)
+
+
+
+create table if not exists 50_1RequestAccepted597(
+    requester_id int,
+    accepter_id int,
+    accept_date date
+);
+
+
+insert into 50_1RequestAccepted597 values 
+(2, 3, '2016-06-08'),
+(1, 2, '2016-06-03'),
+(1, 3, '2016-06-08'),
+(3, 4, '2016-06-09'),
+(3, 4, '2016-06-10');
+
+mysql> select * from 50_1RequestAccepted597;
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+|            2 |           3 | 2016-06-08  |
+|            1 |           2 | 2016-06-03  |
+|            1 |           3 | 2016-06-08  |
+|            3 |           4 | 2016-06-09  |
+|            3 |           4 | 2016-06-10  |
++--------------+-------------+-------------+
+5 rows in set (0.01 sec)
+
+
+
+--Write an SQL query to find the overall acceptance rate of requests, 
+--which is the number of acceptance divided by the number of requests. 
+--Return the answer rounded to 2 decimals places. Note that: 
+--• The accepted requests are not necessarily from the table friend_request. 
+--In this case, Count the total accepted requests (no matter whether they are in the original requests),
+-- and divide it by the number of requests to get the acceptance rate. 
+--• It is possible that a sender sends multiple requests to the same receiver, 
+--and a request could be accepted more than once. 
+--In this case, the 'duplicated' requests or acceptances are only counted once. 
+--• If there are no requests at all, you should return 0.00 as the accept_rate. 
+--The query result format is in the following example.
+
+
+
+select ifnull(round(
+(select count(*) from
+(select distinct requester_id, accepter_id
+from 50_1RequestAccepted597)
+as total_count) /
+(select count(*) from
+(select distinct sender_id,send_to_id
+from 50FrndRqstOverallAcceptanceRate597)
+as total_count2),2),0) as accept_rate
+
+
+Output
+
+
++-------------+
+| accept_rate |
++-------------+
+|        0.80 |
++-------------+
+1 row in set (0.05 sec)
+
+
+
+
+--------------------------------------------------------------------------------------------
+
+
