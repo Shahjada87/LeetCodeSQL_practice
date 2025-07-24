c2835df066a9ddf66be0db7d7db3338cb01783a1
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-07-24 13:10:44 +0100

    SOlved 1082 Sales analysis I on leetcode

--- 55.24July2025Leetcode.sql
+++ 55.24July2025Leetcode.sql
@@ -232,4 +232,92 @@ Output
 ---------------------------------------------------------------------------------------
 
 
+Q. 1082 Sales Analysis I
+
+create table if not exists 55SalesAnalysis1082(
+    product_id int primary key,
+    product_name varchar(50),
+    unit_price int
+);
+
+
+insert into 55SalesAnalysis1082 values 
+(1, 'S8', 1000),
+(2, 'G4', 800),
+(3, 'iPhone', 1400);
+
+
+mysql> select * from 55SalesAnalysis1082;
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+|          1 | S8           |       1000 |
+|          2 | G4           |        800 |
+|          3 | iPhone       |       1400 |
++------------+--------------+------------+
+3 rows in set (0.00 sec)
+
+
+
+
+create table if not exists 54_1Sales1082(
+    seller_id int,
+    product_id int,
+    buyer_id int,
+    sales_date date,
+    quantity int,
+    price int,
+    Foreign key(product_id) REFERENCES 55SalesAnalysis1082(product_id)
+);
+
+
+insert into 54_1Sales1082 values 
+(1, 1, 1, '2019-01-21', 2, 2000),
+(1, 2, 2, '2019-02-17', 1, 800),
+(2, 2, 3, '2019-06-02', 1, 800),
+(3, 3, 4, '2019-05-13', 2, 2800);
+
+mysql> select * from 54_1Sales1082;
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sales_date | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+|         1 |          1 |        1 | 2019-01-21 |        2 |  2000 |
+|         1 |          2 |        2 | 2019-02-17 |        1 |   800 |
+|         2 |          2 |        3 | 2019-06-02 |        1 |   800 |
+|         3 |          3 |        4 | 2019-05-13 |        2 |  2800 |
++-----------+------------+----------+------------+----------+-------+
+4 rows in set (0.00 sec)
+
+rename table 54_1Sales1082 to 55_1Sales1082;
+
+--Write an sql query that reports the best seller by total sales price, 
+--if there is a tie report them all.
+--return the table in any order
+
+
+Select seller_id, Sum(price)
+from 55_1Sales1082
+group by seller_id
+having sum(price) = (
+        select max(total_sales) FROM
+            (
+                select seller_id, sum(price) as total_sales
+                from 55_1Sales1082
+                group by seller_id
+            )
+        as sub_query
+    );
+
+
+Output
+
+
++-----------+------------+
+| seller_id | Sum(price) |
++-----------+------------+
+|         1 |       2800 |
+|         3 |       2800 |
++-----------+------------+
+2 rows in set (0.01 sec)
+
 
