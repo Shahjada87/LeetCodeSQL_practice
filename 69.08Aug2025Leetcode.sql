3b5e1f9cec9e10013b95a6fba725d3b297568a3f
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-08-08 15:21:41 +0100

    resolved 1729 for practice from leetcode

---
+++ 69.08Aug2025Leetcode.sql
@@ -0,0 +1,54 @@
+--Praactice one random question from already solved -- LeetCode problems
+
+Use PracticeRandomSolvedLeetCodeProbs;
+
+Show tables;
+
+Q. 1729 Find Followers Count
+
+create table if not exists 60FindFollowersCount1729(
+    user_id int,
+    follower_id int,
+    primary key (user_id, follower_id)
+);
+
+insert into 60FindFollowersCount1729 values
+(0,1),
+(1,0),
+(2,0),
+(2,1);
+
+mysql> select * from 60FindFollowersCount1729;
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+|       0 |           1 |
+|       1 |           0 |
+|       2 |           0 |
+|       2 |           1 |
++---------+-------------+
+4 rows in set (0.02 sec)
+
+
+
+--Write a solution that will, for each user, return the number of followers.
+--Return the result table ordered by user_id in ascending order.
+
+
+select user_id, count(DISTINCT follower_id) as distinct_follower
+from 60FindFollowersCount1729
+group by user_id
+order by user_id asc;
+
+
+Output 
+
+
++---------+-------------------+
+| user_id | distinct_follower |
++---------+-------------------+
+|       0 |                 1 |
+|       1 |                 1 |
+|       2 |                 2 |
++---------+-------------------+
+3 rows in set (0.01 sec)
